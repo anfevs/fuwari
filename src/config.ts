2832183c0b5b9eabd8932d491daa01987ff446dc
anfevs <3594168593@qq.com>
2025-05-18 20:59:31 +0800

    添加了评论

--- src/config.ts
+++ src/config.ts
@@ -46,11 +46,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		LinkPreset.Friends,  
-		{
-			name: "GitHub",
-			url: "https://github.com/anfevs", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-	},
+//		{
+//			name: "GitHub",
+//			url: "https://github.com/anfevs", // Internal links should not include the base path, as it is automatically added
+//			external: true, // Show an external link icon and will open in a new tab
+//	},
 		],
 	}
 
